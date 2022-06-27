183daa5e845a9bc905a503f9c56922d3abc7d649
Mohit Palriwal <37902584+mohitmahi@users.noreply.github.com>
2022-06-26 18:50:33 -0700

    Update BST.java

--- BST.java
+++ BST.java
@@ -3,42 +3,44 @@ import lombok.Builder;
 public class BST {
 
     public static void main(String[] args) {
-        //   3
-        //  /\
-        // 2  4
-        Node root_left = Node.builder().value(2).build();
-        Node root_right = Node.builder().value(4).build();
-        Node root = Node.builder()
-                .value(3)
-                .left(root_left)
-                .right(root_right)
-                .build();
+
+        Node root = getBST();
         BinarySearchTree tree = BinarySearchTree.builder().root(root).build();
 
         System.out.println("IsPresent:: " + isNumberPresent(tree, 3));
         System.out.println("IsPresent:: " + isNumberPresent(tree, 2));
         System.out.println("IsPresent:: " + isNumberPresent(tree, 4));
-        System.out.println("IsPresent:: " + isNumberPresent(tree, 5));
+        System.out.println("IsPresent:: " + isNumberPresent(tree, 6));
+    }
+
+    private static Node getBST() {
+        //   3
+        //  / \
+        // 2   5
+        //    /\
+        //   4  6
+        return Node.builder()
+                .value(3)
+                .left(Node.builder().value(2)
+                        .build())
+                .right(Node.builder().value(5)
+                        .left(Node.builder().value(4).build())
+                        .right(Node.builder().value(6).build())
+                        .build())
+                .build();
     }
 
     private static boolean isNumberPresent(BinarySearchTree tree, int number) {
         if (tree == null) return false;
 
-        return DFSAndFind(tree.root, number);
+        return DFSAndFind(tree.root, number); //(OLogN)
     }
 
     private static boolean DFSAndFind(Node root, int number) {
         if(root == null) return  false;
-
         if(root.value == number)  return true;
-
-        if(root.value > number) {
-            return DFSAndFind(root.left, number);
-        } else {
-            return DFSAndFind(root.right, number);
-        }
+        return root.value > number ? DFSAndFind(root.left, number) : DFSAndFind(root.right, number);
     }
-
 
     @Builder
     static class BinarySearchTree {
