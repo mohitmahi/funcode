6eb007b068e1dc7c47c56650cfc7a281d020c5c7
mpalriwal <mpalriwal@salesforce.com>
2022-07-10 01:06:40 -0700

    Three Sum

---
+++ src/main/java/ThreeSum.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+// 2 SUM => K => Use a HashMap/Set or Left/Right Pointer
+// 3 SUM => 0 => Pivot and then find 2 sum which is complement of pivot
+public class ThreeSum {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Set<List<Integer>> res = new HashSet<>();
+        Set<Integer> dups = new HashSet<>();
+        Map<Integer, Integer> seen = new HashMap<>();
+        for (int i = 0; i < nums.length; ++i)
+            if (dups.add(nums[i])) {
+                for (int j = i + 1; j < nums.length; ++j) {
+                    int complement = -nums[i] - nums[j];
+                    if (seen.containsKey(complement) && seen.get(complement) == i) {
+                        List<Integer> triplet = Arrays.asList(nums[i], nums[j], complement);
+                        Collections.sort(triplet);
+                        res.add(triplet);
+                    }
+                    seen.put(nums[j], i);
+                }
+            }
+        return new ArrayList(res);
+    }
+}
+
