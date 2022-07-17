30c0dae72f0c6317cd052cc7618b5395a1cf66a1
mpalriwal <mpalriwal@salesforce.com>
2022-07-16 23:19:43 -0700

    Max Product Per Level

---
+++ src/main/java/MaxPerLevelProduct.java
@@ -0,0 +1,63 @@
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MaxPerLevelProduct {
+
+    public static void main(String[] args) {
+        final TreeNode root = TreeNode.builder().value(10)
+                .left(TreeNode.builder()
+                        .value(3)
+                        .left(TreeNode.builder()
+                                .value(7)
+                                .build())
+                        .right(TreeNode.builder()
+                                .value(10)
+                                .build())
+                        .build())
+                .right(TreeNode.builder()
+                        .value(4)
+                        .left(TreeNode.builder()
+                                .value(12)
+                                .build())
+                        .right(TreeNode.builder()
+                                .value(9)
+                                .build())
+                        .build())
+                .build();
+
+        System.out.println(findMaxProduct(root));
+    }
+
+    private static int findMaxProduct(TreeNode root) {
+        final Queue<TreeNode> queue = new LinkedList<>();
+        if(root == null) return -1;
+
+        queue.add(root);
+        int result = 1;
+        while (!queue.isEmpty()) {
+            int qSize = queue.size();
+            int max = Integer.MIN_VALUE;
+            while (qSize-- > 0) { //Total item at current Level  "L"
+                final TreeNode current = queue.poll();
+                max = Math.max(max, current.value);
+
+                if(current.left != null) queue.add(current.left);
+                if(current.right != null) queue.add(current.right);
+            }
+            result *= max;
+        }
+        return result;
+    }
+
+
+    @Builder
+    @Data
+    static class TreeNode {
+        int value;
+        TreeNode left;
+        TreeNode right;
+    }
+}
