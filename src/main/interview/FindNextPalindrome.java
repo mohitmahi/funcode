f89f103851f1c84f67b7fbe8a0b750b7d2e8cd13
mpalriwal <mpalriwal@salesforce.com>
2022-07-22 18:05:50 -0700

    FindNexPalindrome

--- src/main/interview/FindNextPalindrome.java
+++ src/main/interview/FindNextPalindrome.java
@@ -3,21 +3,26 @@ import org.junit.Assert;
 public class FindNextPalindrome {
     public static void main(String[] args) {
         Assert.assertEquals(2, findNextPalindrome(1));
-        Assert.assertEquals(191,findNextPalindrome(9));
-        Assert.assertEquals(929,findNextPalindrome(923));
-        Assert.assertEquals(131,findNextPalindrome(123));
-        Assert.assertEquals(101,findNextPalindrome(99));
-        Assert.assertEquals(2002,findNextPalindrome(2000));
-        Assert.assertEquals(3003,findNextPalindrome(2999));
+        Assert.assertEquals(11,findNextPalindrome(9));
+
+        Assert.assertEquals(1001,findNextPalindrome(999)); //odd with one more digit
+        Assert.assertEquals(929,findNextPalindrome(923)); //odd without adding one to Left half
+        Assert.assertEquals(131,findNextPalindrome(123)); //odd with adding one to Left half
+
+        Assert.assertEquals(10001,findNextPalindrome(9999)); //even with one more digit
+        Assert.assertEquals(3003,findNextPalindrome(2999)); //even with adding one to Left half
+        Assert.assertEquals(2002,findNextPalindrome(2000)); //even without adding one to Left half
     }
 
     private static int findNextPalindrome(int input) {
         final String inputS = String.valueOf(input);
-        final int size = inputS.length();
-        if (input < 9) return input + 1;
 
+        if (input < 9) return input + 1;
+        if (input == 9) return  11; //corner case
+
+        final int size = inputS.length();
         boolean isEven = size%2 == 0;
-        final String leftHalf = input == 9 ? "1" : inputS.substring(0, size / 2);
+        final String leftHalf =  inputS.substring(0, size / 2);
         StringBuilder temp = new StringBuilder(leftHalf);
         final String rightHalf = String.valueOf(temp.reverse());
 
@@ -38,7 +43,8 @@ public class FindNextPalindrome {
             leftAddedOne = String.valueOf(Integer.parseInt(leftHalf + inputS.charAt(size / 2)) + 1); //123 => 1(2+1) => 131
         }
         temp = new StringBuilder(leftAddedOne);
-        if(leftAddedOne.length() > leftHalf.length()) { //99 => 9-9 => 10=> 10-1X(Deleted) => 101
+        int diff = leftAddedOne.length() - leftHalf.length();
+        while(diff-- > 0) { //99 => 9-9 => 10=> 10-1X(Deleted) => 101
             temp.deleteCharAt(temp.length() - 1);
         }
         final String rightHalfNew = String.valueOf(temp.reverse());
