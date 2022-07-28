26821e5dde64f4b06583d055522aa1c929ac1723
mpalriwal <mpalriwal@salesforce.com>
2022-07-28 12:11:27 -0700

    B75

--- src/main/java/ThreeSum.java
+++ src/main/java/ThreeSum.java
@@ -9,19 +9,19 @@ import java.util.Set;
 
 //https://leetcode.com/problems/3sum/solution/
 // 2 SUM => K => Use a HashMap/Set or Left/Right Pointer
-// 3 SUM => 0 => Pivot and then find 2 sum which is complement of pivot
+// 3 SUM => 0 => Pivot and then find 2 sum which is complement of pivot //O(n * n) and space O(n)
 public class ThreeSum {
     public List<List<Integer>> threeSum(int[] nums) {
         Set<List<Integer>> res = new HashSet<>();
         Set<Integer> dups = new HashSet<>();
         Map<Integer, Integer> seen = new HashMap<>();
         for (int i = 0; i < nums.length; ++i)
-            if (dups.add(nums[i])) {
+            if (dups.add(nums[i])) { //skip duplicate
                 for (int j = i + 1; j < nums.length; ++j) {
                     int complement = -nums[i] - nums[j];
-                    if (seen.containsKey(complement) && seen.get(complement) == i) {
+                    if (seen.containsKey(complement) && seen.get(complement) == i) { //  indicate whether we have encountered that element in the current iteration.
                         List<Integer> triplet = Arrays.asList(nums[i], nums[j], complement);
-                        Collections.sort(triplet);
+                        Collections.sort(triplet); // just to avoid duplicate with same triplet
                         res.add(triplet);
                     }
                     seen.put(nums[j], i);
